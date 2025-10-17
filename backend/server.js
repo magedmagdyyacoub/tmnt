cff4f8593d611c3a263485bdb73158a6b4d4d353
magedmagdyydacoub <magedmagdyyacoub3@gmail.com>
2025-10-17 13:07:01 +0300

    Configured backend for Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,5 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/posts', postsRoutes);
 app.use('/api/comments', commentsRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
